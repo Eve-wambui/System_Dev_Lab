7127f504902af9e574ac3f7d2dba37ea9c16e0c5
Eve-wambui <wambui17evelyne@gmail.com>
2022-12-19 17:02:50 +0300

    register and login success authentication

--- app/src/main/java/com/example/meetuphubapp/Login.java
+++ app/src/main/java/com/example/meetuphubapp/Login.java
@@ -1,5 +1,6 @@
 package com.example.meetuphubapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,37 +10,99 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    //create db object DatabaseReference to access firebase realtime db
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://meetuphub-2c305-default-rtdb.firebaseio.com");
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
+        final EditText email =(EditText) findViewById(R.id.emailET);
+        final EditText password =(EditText) findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        final MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        TextView registerInstead = (TextView) findViewById(R.id.registerInstead);
+        final TextView registerInstead = (TextView) findViewById(R.id.registerInstead);
 
-        //admin and admin as credentials
+        firebaseAuth = FirebaseAuth.getInstance();
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct
-                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                    //creating intent
-                    Intent timeline = new Intent(Login.this, Timeline.class);
-                    //starting home feed
-                    startActivity(timeline);
-                }else
-                    //incorrect
-                    Toast.makeText(Login.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+
+                final String emailTxt = email.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(emailTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(Login.this,"Please input username or password",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    /**
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //check if username exists
+                            if (snapshot.hasChild(usernameTxt)){
+
+                                // confirm password matches
+                                final String getPassword = snapshot.child("password").getValue(String.class);
+
+                                if (getPassword.equals(passwordTxt)){
+                                    Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                    //creating intent
+                                    Intent timeline = new Intent(Login.this, Timeline.class);
+                                    //starting home feed
+                                    startActivity(timeline);
+                                } else {
+                                    Toast.makeText(Login.this,"Incorrect password",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(Login.this,"Incorrect password",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                    */
+
+                    firebaseAuth.signInWithEmailAndPassword(emailTxt,passwordTxt)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    //creating intent
+                                    Intent home = new Intent(Login.this, Timeline.class);
+                                    //starting registration activity
+                                    startActivity(home);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Login.this, "Error: " +e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+
             }
         });
 
--- app/src/main/java/com/example/meetuphubapp/Registration.java
+++ app/src/main/java/com/example/meetuphubapp/Registration.java
@@ -1,43 +1,126 @@
 package com.example.meetuphubapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Registration extends AppCompatActivity {
+
+    //create db object DatabaseReference to access firebase realtime db
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://meetuphub-2c305-default-rtdb.firebaseio.com");
+
+    FirebaseAuth firebaseAuth;
+     private EditText regusername, emailET,regpassword, repassword;
+     private MaterialButton regbtn;
+     private String regusernameTxt, emailTxt, regpasswordTxt, repasswordTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        TextView regusername =(TextView) findViewById(R.id.regusername);
-       //TextView email =(TextView) findViewById(R.id.email);
-        TextView regpassword =(TextView) findViewById(R.id.regpassword);
-       // TextView repassword =(TextView) findViewById(R.id.repassword);
+         regusername =(EditText) findViewById(R.id.regusername);
+         emailET =(EditText) findViewById(R.id.email);
+         regpassword =(EditText) findViewById(R.id.regpassword);
+         repassword =(EditText) findViewById(R.id.repassword);
 
-        MaterialButton regbtn = (MaterialButton) findViewById(R.id.regbtn);
+         regbtn = (MaterialButton) findViewById(R.id.regbtn);
+         firebaseAuth = FirebaseAuth.getInstance();
 
         regbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(regusername.getText().toString().equals("wambui") && regpassword.getText().toString().equals("wambui")){
-                    //correct
-                    Toast.makeText(Registration.this,"REGISTRATION SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                    //creating intent
-                    Intent feed = new Intent(Registration.this, Timeline.class);
-                    //starting home feed
-                    startActivity(feed);
-                }else
-                    //incorrect
-                    Toast.makeText(Registration.this,"REGISTRATION FAILED! TRY AGAIN",Toast.LENGTH_SHORT).show();
+
+                // get data from EditTexts into string variables
+                 regusernameTxt = regusername.getText().toString();
+                 emailTxt = emailET.getText().toString();
+                 regpasswordTxt = regpassword.getText().toString();
+                 repasswordTxt = repassword.getText().toString();
+
+                // check if all fields are filled before sending to firebase
+                if(regusernameTxt.isEmpty() || emailTxt.isEmpty() || regpasswordTxt.isEmpty()) {
+                    Toast.makeText(Registration.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+
+                //check if passwords match
+                else if(!regpasswordTxt.equals(repasswordTxt)){
+                    Toast.makeText(Registration.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+
+                        authenticate ();
+
+
+                }
             }
         });
+    }
+
+    private void authenticate() {
+        firebaseAuth.createUserWithEmailAndPassword(emailTxt, regpasswordTxt)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                       //save data
+                       saveData();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Registration.this, "Error: " +e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void saveData() {
+        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //check if username is registered before
+                if (snapshot.hasChild(regusernameTxt)){
+                    Toast.makeText(Registration.this, "Username already exists. Pick another", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+
+                    //send data to db
+                    //username is unique identifier; all data comes under username
+                    databaseReference.child("users").child(regusernameTxt).child("email").setValue(emailTxt);
+                    databaseReference.child("users").child(regusernameTxt).child("password").setValue(regpasswordTxt);
+
+                    Toast.makeText(Registration.this, "User registered Successfully!", Toast.LENGTH_SHORT).show();
+                    //creating intent
+                    Intent loginpage = new Intent(Registration.this, Login.class);
+                    //starting login page
+                    startActivity(loginpage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
