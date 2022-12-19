cde0d6d9fc1232aef32f5dd8d010876f49795ed4
Eve-wambui <wambui17evelyne@gmail.com>
2022-12-19 23:10:01 +0300

    successfully uploaded image to firebase storage

--- app/src/main/java/com/example/meetuphubapp/home/ExperienceFragment.java
+++ app/src/main/java/com/example/meetuphubapp/home/ExperienceFragment.java
@@ -1,20 +1,123 @@
 package com.example.meetuphubapp.home;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.meetuphubapp.R;
+import com.example.meetuphubapp.databinding.FragmentExperienceBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ExperienceFragment extends Fragment {
+
+    FragmentExperienceBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    //ProgressDialog progressDialog;
+
+    private ImageView firebaseimage;
+    private MaterialButton selectimage, uploadimage;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_experience, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding = FragmentExperienceBinding.bind(view);
+
+        binding.selectimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        binding.uploadimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void uploadImage() {
+
+//        progressDialog = new ProgressDialog(getActivity());
+//        progressDialog.setTitle("Uploading Image ...");
+//        progressDialog.show();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_ss", Locale.US);
+        Date now = new Date();
+        String filename  = formatter.format(now);
+
+        storageReference = FirebaseStorage.getInstance().getReference("images/" + filename);
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        binding.firebaseimage.setImageURI(null);
+                        Toast.makeText(ExperienceFragment.super.getContext(),
+                                "Image successfully uploaded",
+                                Toast.LENGTH_SHORT).show();
+//                        if (progressDialog.isShowing())
+//                            progressDialog.dismiss();
+
+                    }
+                }) .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ExperienceFragment.super.getContext(),
+                                "Image upload failed",
+                                Toast.LENGTH_SHORT).show();
+//                        if (progressDialog.isShowing())
+//                            progressDialog.dismiss();
+                    }
+                });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null){
+            imageUri = data.getData();
+            binding.firebaseimage.setImageURI(imageUri);
+        }
     }
 }
